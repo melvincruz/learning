052a84b0578901e7685f47c0bcdf1c23181f438b
Melvin Cruz <cruz97.m@gmail.com>
2015-10-15 14:02:01 -0400

    quiz update

--- quiz.rb
+++ quiz.rb
@@ -5,10 +5,6 @@ puts "Hello, " + name
 #puts "Press enter to begin the quiz."
 #gets
 score = 0
-
-
-
-
 def ask_question(question, correct_answer)
   puts question
   answer = gets.chomp
@@ -20,20 +16,19 @@ def ask_question(question, correct_answer)
     return false
   end
 end
-if ask_question("what is 2+2", "4")
-   score = score + 1
+questions = [["what is 2+2","4"], ["what is 2+3","5"], ["what is 2+4","6"], ["what is 2+5","7"], ["what is 2+6","8"]]
+questions.each do |x|
+  if ask_question(x[0], x[1])
+  score = score +1
+  end
 end
-if ask_question("what is 2+3", "5")
-  score = score + 1
-end
-if ask_question("what is 2+4", "6")
-  score = score + 1
-end
-if ask_question("what is 2+5", "7")
-  score = score + 1
-end
-if ask_question("what is 2+6", "8")
-  score = score + 1
+
+
+for x in (0..4)
+  if ask_question(questions[x], answers[x])
+  score = score +1
+  end
 end
 puts "You got " + score.to_s + " questions out of " + 5.to_s
 puts score/5.0*100
+
