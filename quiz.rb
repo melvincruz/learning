1a14b5539db42ad02cbb78956fd8141be8aa1051
Melvin Cruz <cruz97.m@gmail.com>
2015-10-22 12:48:39 -0400

    revision

--- quiz.rb
+++ quiz.rb
@@ -1,3 +1,36 @@
+class Question
+  attr_reader :question, :answer
+  def initialize(question, answer)
+    @question = question
+    @answer = answer
+  end
+
+
+  def self.make_addition(n1, n2)
+    return Question.new("What is " + n1.to_s + "+" + n2.to_s + "?", (n1+n2).to_s)
+  end
+
+  def self.make_random_addition
+    n1 = rand(10)
+    n2 = rand(10)
+    return Question.new("What is " + n1.to_s + "+" + n2.to_s + "?", (n1+n2).to_s)
+ end
+
+
+  def ask
+    puts @question
+    input = gets.chomp
+    if input == @answer
+      puts "correct"
+      return true
+    else
+      puts "Wrong! The answer is " + @answer
+      return false
+    end
+  end
+end
+
+
 puts "Welcome to the Ruby Arithmatic Tutor"
 puts "what is your name?"
 name = gets
@@ -5,30 +38,18 @@ puts "Hello, " + name
 #puts "Press enter to begin the quiz."
 #gets
 score = 0
-def ask_question(question, correct_answer)
-  puts question
-  answer = gets.chomp
-  if answer == correct_answer
-    puts "correct"
-    return true
-  else
-    puts "Wrong! The answer is " + correct_answer
-    return false
-  end
-end
-questions = [["what is 2+2","4"], ["what is 2+3","5"], ["what is 2+4","6"], ["what is 2+5","7"], ["what is 2+6","8"]]
-questions.each do |x|
-  if ask_question(x[0], x[1])
-  score = score +1
+
+questions = 3
+
+questions.times do 
+  x = Question.make_random_addition
+  if x.ask
+    score = score+1
   end
 end
 
 
-for x in (0..4)
-  if ask_question(questions[x], answers[x])
-  score = score +1
-  end
-end
-puts "You got " + score.to_s + " questions out of " + 5.to_s
-puts score/5.0*100
+puts "You got " + score.to_s + " questions out of " + questions.to_s
+puts score/ questions.to_f*100
+
 
