2e8238fbce05131804f8aa4ade1bb6a1639d377b
Melvin Cruz <cruz97.m@gmail.com>
2015-10-29 11:12:16 -0400

    finaly done

--- guess.rb
+++ guess.rb
@@ -5,37 +5,36 @@ class SecretNumber
 	end
 
 	def self.random_secret
-		secret = rand(10000)
+		SecretNumber.new(rand(10000))
 	end
 
 	def guessing
-		input = gets.chomp
-		loop do
-			if input == @secret
-				puts "Correct! Nice job."
-				return true
-			else input > @secret
-				puts "To high, Guess again."
-				 input < @secret
-				puts "To low, Guess again."
-				return false
-			end
+		input = gets.chomp.to_i
+		if input == @secret
+			puts "Correct! Nice job."
+			return true			
+		elsif input > @secret
+			puts "Too high, Guess again."
+			return false
+		elsif input < @secret 
+			puts "Too low, Guess again."
+			return false
 		end
 	end
 end
 
 puts "Welcome to my guessing game!, " 
-puts "I am thinking of a number, Start Guessing!"
+puts "I am thinking of a number, press enter to Start Guessing!"
+
+correct = false
 
 tries = 0
 
-think = 1
+secret = SecretNumber.random_secret
 
- think.times do
-	x = SecretNumber.random_secret
-	if x.guessing
-		tries = tries+1
-	end
+while  correct == false do
+	correct = secret.guessing
+	tries = tries+1
 end
 
-puts "Finally you Got it it, " + secret.to_s + " was the number, and it only took you " + tries.to_s + "tries!"
+puts "Finally you Got it it, " + secret.secret.to_s + " was the number, and it only took you " + tries.to_s + " tries!"
